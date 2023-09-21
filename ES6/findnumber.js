f64e3c5e1d2bdbe0e80bab3a7f9bacd969a0af8d
aankit09 <aankitsanvedi@gmail.com>
2023-09-22 00:23:14 +0530

    Add

---
+++ ES6/findnumber.js
@@ -0,0 +1,11 @@
+
+// let value = [10,20,35,40,55,70];
+// let result=  value.find(function(item) {
+//     return item<60;
+// });
+let value1 = [10,20,35,40,55,70];
+let result1=  value1.findIndex(function(item) {
+    return item=>60;
+});
+//console.log(result);
+console.log(result1);
