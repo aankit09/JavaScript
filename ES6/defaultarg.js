610f85aacaa1e636574a86b7a8c2d9c34425c814
aankit09 <aankitsanvedi@gmail.com>
2023-09-21 23:44:22 +0530

    Some Practice Added

--- ES6/defaultarg.js
+++ ES6/defaultarg.js
@@ -1,4 +1,21 @@
+//First Example Console Value 
 function multi(a,b) {
     return a+b;
 }
 console.log(multi(10,2));
+console.log(multi(6,33));
+
+// Second Example Send Value
+function addition() {
+    return 50+90;
+}
+console.log(addition());
+
+
+// Thrid Example One value defined and one value get
+
+function add(a,b=23)
+{
+       return a+b;
+}
+console.log(add(6));
--- ES6/variable.js
+++ ES6/variable.js
@@ -5,3 +5,15 @@ const cast = "Rajput";
 console.log(myName);
 console.log(mySurname);
 console.log(cast);
+
+let name = "check let";
+
+if (true) {
+    let name="final"
+    console.log(name);
+}
+console.warn(name);
+
+for(var i=1;i<=10;i++){
+    console.warn(i);
+}
