59308ca3ac572b4302dc044594a7bb3abd0b953a
aankit09 <aankitsanvedi@gmail.com>
2023-09-22 00:10:54 +0530

    Added

--- ES6/tamplatelitretrl.js
+++ ES6/tamplatelitretrl.js
@@ -1,6 +1,9 @@
 for (let num = 1; num < 10; num++){
     let tableof = 12;
     console.log(`${tableof} * ${num} = ${tableof * num}`);
-
-1   
 }
+
+let quotes = "ES6 full course in Hindi ECMA script 6 full tutoriallo ";
+let auther = "ankit";
+console.log(`${quotes}
+${auther}`);
