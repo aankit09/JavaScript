11bb3a68045811fc92a3ed9c6df68716aa7d073a
aankit09 <aankitsanvedi@gmail.com>
2023-09-21 19:58:27 +0530

    some new challenges added

---
+++ array/array.js
@@ -0,0 +1,14 @@
+
+//var myFriends=["ankit",30,"Anup",29,"Prayashu",29,"Nishant",31];
+
+// Travarsal Array
+
+var myFriends=["ankit","Anup","Prayashu","Nishant"];
+//console.log(myFriends[1]);
+// console.log(myFriends.length);
+// console.log(myFriends[myFriends.length -1]);
+
+for (var index = 0; index < myFriends.length; index++) {
+    console.log(myFriends[index]);
+    
+}
---
+++ array/logical.js
@@ -0,0 +1,5 @@
+const month = ['jan','feb','april','may','july', 'sep'];
+const newMonth = month.splice(2,0,"march");
+const addMonth = month.splice(month.length,5,"june",); 
+const countMonth= 
+console.log(month);
